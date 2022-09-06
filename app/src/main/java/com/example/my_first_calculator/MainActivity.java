084b5a4d264d8972fe231306cabdd9a0b0ce3546
Alejo9601 <alejandro96jm@gmail.com>
2022-09-06 16:42:51 -0300

    Adde functionality to operate just to numbers

--- app/src/main/java/com/example/my_first_calculator/MainActivity.java
+++ app/src/main/java/com/example/my_first_calculator/MainActivity.java
@@ -19,10 +19,44 @@ public class MainActivity extends AppCompatActivity {
         tv_screen = (TextView) findViewById(R.id.tv_screen);
     }
 
-    public void setTextOnScreen(View view){
+    public void setNumber(View view){
         Button btn = (Button) view;
         String numToSet = btn.getText().toString();
         tv_screen.setText(tv_screen.getText() + numToSet);
+    }
+
+    public void setOperator(View view){
+        Button btn = (Button) view;
+        String op = btn.getText().toString();
+        tv_screen.setText(tv_screen.getText() + " " + op + " ");
+    }
+
+
+    public void calculate (View view) {
+        String expression = tv_screen.getText().toString();
+
+        String[] splitedExpression = expression.split(" ");
+
+        int num1 = Integer.parseInt(splitedExpression[0]);
+        String operator = splitedExpression[1];
+        int num2 = Integer.parseInt(splitedExpression[2]);
+
+        switch (operator) {
+            case "+":
+                tv_screen.setText(String.valueOf(num1 + num2));
+                break;
+            case "-":
+                tv_screen.setText(String.valueOf(num1 - num2));
+                break;
+            case "*":
+                tv_screen.setText(String.valueOf(num1 * num2));
+                break;
+            case "/":
+                tv_screen.setText(String.valueOf(num1 / num2));
+                break;
+            default:
+                break;
+        }
     }
 
 }
