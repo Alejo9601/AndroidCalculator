ff6cafdeed651f0050bf15e34484100245cd3b78
Alejo9601 <alejandro96jm@gmail.com>
2022-09-07 00:32:57 -0300

    Refactored code

--- app/src/main/java/com/example/my_first_calculator/MainActivity.java
+++ app/src/main/java/com/example/my_first_calculator/MainActivity.java
@@ -6,10 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tv_screen;
+    private String op;
+    private float num1;
+    private float num2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,42 +21,88 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tv_screen = (TextView) findViewById(R.id.tv_screen);
+        num1 = 0;
+        num2 = 0;
+        op = "";
     }
 
-    public void setNumber(View view){
+    public void setNum1 (float num) {
+        this.num1 = num;
+    }
+
+    public void setNum2 (float num) {
+        this.num2 = num;
+    }
+
+    public float getNum1() {
+        return this.num1;
+    }
+
+    public float getNum2() {
+        return this.num2;
+    }
+
+    public void setOp(String op) {
+        this.op = op;
+    }
+
+    public String getOp() {
+        return this.op;
+    }
+
+    public void setNumberOnScreen(View view){
         Button btn = (Button) view;
         String numToSet = btn.getText().toString();
-        tv_screen.setText(tv_screen.getText() + numToSet);
+
+        tv_screen.setText(tv_screen.getText() + numToSet); //setting number on screen
+
+        if(getOp() == "") {
+            if(getNum1() != 0) numToSet = String.valueOf(getNum1()) + numToSet; //num1 has some value in it
+            setNum1(Float.parseFloat(numToSet));
+        } else {
+            if(getNum2() != 0) numToSet = String.valueOf(getNum2()) + numToSet; //num2 has some value in it
+            setNum2(Float.parseFloat(numToSet));
+        };
+
     }
 
-    public void setOperator(View view){
+    public void setOperatorOnScreen(View view){
         Button btn = (Button) view;
         String op = btn.getText().toString();
+
+        setOp(op); //setting global operator
+
         tv_screen.setText(tv_screen.getText() + " " + op + " ");
     }
 
+    private void onAfterOp(float newNumber) {
+        setNum1(newNumber);
+        setNum2(0);
+        setOp("");
+    }
 
     public void calculate (View view) {
-        String expression = tv_screen.getText().toString();
-
-        String[] splitedExpression = expression.split(" ");
-
-        int num1 = Integer.parseInt(splitedExpression[0]);
-        String operator = splitedExpression[1];
-        int num2 = Integer.parseInt(splitedExpression[2]);
-
-        switch (operator) {
+        float newNumber;
+        switch (getOp()) {
             case "+":
-                tv_screen.setText(String.valueOf(num1 + num2));
+                newNumber = getNum1() + getNum2();
+                tv_screen.setText(String.valueOf(newNumber));
+                onAfterOp(newNumber);
                 break;
             case "-":
-                tv_screen.setText(String.valueOf(num1 - num2));
+                newNumber = getNum1() - getNum2();
+                tv_screen.setText(String.valueOf(newNumber));
+                onAfterOp(newNumber);
                 break;
             case "*":
-                tv_screen.setText(String.valueOf(num1 * num2));
+                newNumber = getNum1() * getNum2();
+                tv_screen.setText(String.valueOf(newNumber));
+                onAfterOp(newNumber);
                 break;
             case "/":
-                tv_screen.setText(String.valueOf(num1 / num2));
+                newNumber = getNum1() / getNum2();
+                tv_screen.setText(String.valueOf(newNumber));
+                onAfterOp(newNumber);
                 break;
             default:
                 break;
